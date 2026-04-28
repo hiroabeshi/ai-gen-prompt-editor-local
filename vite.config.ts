0e2c13833bb752b8a5835c2edc96f981fc24c41a
hiroabeshi <256374532+hiroabeshi@users.noreply.github.com>
2026-04-28 22:25:57 +0900

    fix: set GitHub Pages base for local repo

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
-    base: '/ai-gen-prompt-editor/',
+    base: '/ai-gen-prompt-editor-local/',
     plugins: [
         UnoCSS(),
         vue(),
